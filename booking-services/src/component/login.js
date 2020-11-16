0e62cb379ee78a169740f568267a716cd3f49671
irfaul fadllillah <tadts.17@gmail.com>
2020-11-16 05:25:38 +0000

    Upload New File

---
+++ booking-services/src/component/login.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+
+class Login extends Component {
+
+    render() {
+       return (
+        <form>
+            <h3>Login</h3>
+
+            <div className="form-group">
+                <label>Email</label>
+                <input type="email" className="form-control" placeholder="Masukkan email" />
+            </div>
+
+            <div className="form-group">
+                <label>Password</label>
+                <input type="password" className="form-control" placeholder="Masukkan password" />
+            </div>
+
+            <button className="btn btn-login">Login</button>
+        </form>
+       )
+    }
+}
+
+export default Login;
