51e813e5e3edcba702a4e388e93c0f6ae7382621
kalyaalifa <kalyaalifa@gmail.com>
2020-11-17 07:43:51 +0700

    menambahkan route di App.js bpba

--- booking-services/src/App.js
+++ booking-services/src/App.js
@@ -3,6 +3,8 @@ import './style/bootstrap.min.css';
 import logo from './assets/logo-brimo.png';
 import Login from './component/login';
 import Home from './pages/bpba/home';
+import Tambah from './pages/bpba/tambahUser';
+import Edit from './pages/bpba/editUser';
 import {
   BrowserRouter as Router,
   Switch,
@@ -16,6 +18,12 @@ function App() {
         <Switch>
             <Route path="/bpba/home">
               <Home />
+            </Route>
+            <Route path="/bpba/tambahUser">
+              <Tambah/>
+            </Route>
+            <Route path="/bpba/editUser">
+              <Edit/> 
             </Route>
             <Route path="/">
               <img src={logo} className="login-logo" alt="Brimo"/>
