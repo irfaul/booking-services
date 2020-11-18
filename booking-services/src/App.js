dd175c0342f80fa1d22e03b09667049b9fb0368f
irfaul <muhirfaul@gmail.com>
2020-11-19 06:42:42 +0700

    tambah routing pada bpba

--- booking-services/src/App.js
+++ booking-services/src/App.js
@@ -4,12 +4,16 @@ import Login from "./pages/login";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import DetailUserPba from "./pages/bpba/detail-user-pba";
 import Tambah from "./pages/bpba/tambahUser";
+import TambahLibur from "./pages/bpba/tambah-libur";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
+          <Route path="/bpba/tambah-libur" exact>
+            <TambahLibur />
+          </Route>
           <Route path="/bpba/tambah-user" exact>
             <Tambah />
           </Route>
