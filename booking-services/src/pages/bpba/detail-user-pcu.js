83caa76fca15e9f5130f09a95f2693ba28fd1271
wisthaadb <wistha.adhika@gmail.com>
2020-11-19 00:12:11 +0700

    Revisi detail-user-pba,pbam,pcu & css detail-user

--- booking-services/src/pages/bpba/detail-user-pcu.js
+++ booking-services/src/pages/bpba/detail-user-pcu.js
@@ -7,16 +7,18 @@ class DetailUserPcu extends Component {
         const { showing } = this.state;
        return (
         <div className="container detail-frame">
-        <h3 style={{marginBottom:'20px'}}>Detail User</h3>
+        <h3 style={{marginBottom:'20px', textAlign:'center'}}>Detail User</h3>
         <div className="row">
             <div className="col-md-4">
                 <img alt="" style={{width:'200'}} title="" className="img-circle img-thumbnail isTooltip rounded" src="https://bootdey.com/img/Content/avatar/avatar7.png" data-original-title="Usuario"/> 
             </div>
-            <div className="col-md-6 text-left">
+            <div className="col-md-8 text-left">
                 <strong>Revi</strong><br></br>
                 <p>revi123@gmail.com</p>
-            </div>
-            <div className="col-md-4">PCu</div>
+            </div>   
+        </div>
+        <div className="row">
+            <div className="col-sm-4 text-center">PCu</div>
         </div>
 
         <div className="row rounded" style={{borderStyle: 'solid', borderWidth: 'thin', borderColor:'Gainsboro',margin:'20px 0 20px 0'}}>
@@ -45,12 +47,13 @@ class DetailUserPcu extends Component {
         <div className="form-group" style={{marginTop:'20px'}}>
         { showing 
             ?<input type="text" className="form-control" id="inputText" aria-describedby="textAlasan" placeholder="Alasan disable"/> 
-            :<p className="text-left font-weight-light">Placeholder Alasan</p>
+            :<p className="text-left font-weight-light">Placeholder</p>
         }
         </div>
         {/* button ubah */}
         <div className="btn-detail"> 
-            <button type="button" className="btn btn-outline" style={{backgroundColor: '#F37024'}} onClick={() => this.setState({ showing: false })}>Ubah</button>
+            <button type="button" className="btn btn-outline" style={{backgroundColor: '#F37024'}}>Ubah</button>
+            <button type="button" className="btn btn-outline" style={{backgroundColor: '#2568A9'}} onClick={() => this.setState({ showing: false })}>Simpan</button>
         </div>
         </div>
        )
