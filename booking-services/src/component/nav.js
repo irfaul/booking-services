a11106b49c7f1959bbbd67134181b3448948016b
maturidi <maturfaishol@gmail.com>
2020-11-18 22:53:33 +0700

    17 November 2020, Login Push v2

--- booking-services/src/component/nav.js
+++ booking-services/src/component/nav.js
@@ -1,21 +1,12 @@
-import React, {useState} from 'react';
+import React from 'react';
 import logo from '../assets/logo-brimo.png';
 import DrawerMenu from './drawer-menu';
 
 //materials core
-import { makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
-import Button from '@material-ui/core/Button';
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
 import IconButton from '@material-ui/core/IconButton';
 
 //material icons
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
 import MenuIcon from '@material-ui/icons/Menu';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 
