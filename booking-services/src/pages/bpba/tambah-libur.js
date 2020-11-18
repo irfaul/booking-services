4d0573cef1d4847599254e063914e72b3101b921
kalyaalifa <kalyaalifa@gmail.com>
2020-11-18 12:52:09 +0700

    halaman tambah libur

---
+++ booking-services/src/pages/bpba/tambah-libur.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import '../../style/bpba-tambah.css';
+
+class TambahLibur extends Component {
+    render() {
+
+       return (
+        <div>
+            <h3>Tambah Libur</h3>
+            <div className="auth-wrapper">
+                <div className="auth-inner">
+                    <form>
+                        <div className="form-group">
+                            <label>Nama</label>
+                            <input type="text" className="form-control" placeholder="Masukkan email" />
+                        </div>
+
+                        <div className="form-group">
+                            <label>Tanggal Mulai</label>
+                            <input type="date" className="form-control" placeholder="Masukkan password" />
+                        </div>
+
+                        <div className="form-group">
+                            <label>Tanggal Selesai</label>
+                            <input type="date" className="form-control" placeholder="Masukkan password" />
+                        </div>
+
+                        <button className="btn btn-login">Simpan</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+       )
+    }
+}
+
+export default TambahLibur;
