28a7d19ae62a9a6aae3087697b1389f1efa0b2e8
kalyaalifa <kalyaalifa@gmail.com>
2020-11-19 13:52:40 +0700

    halaman atur jadwal pba dan laporan kinerja

---
+++ booking-services/src/pages/pbam/atur-jadwal.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import JadwalData from '../../component/jadwal-data';
+
+class AturJadwal extends Component {
+    render() {
+
+       return (
+        <div>
+            <h3>Atur Jadwal</h3>
+            <div>
+                <div>
+                
+                <JadwalData/>
+                </div>
+            </div>
+        </div>
+       )
+    }
+}
+
+export default AturJadwal;
