b93e900930fc0962053692bacae0996331f1d9c8
irfaul <muhirfaul@gmail.com>
2020-11-20 20:36:10 +0700

    nambah routing

--- booking-services/src/App.js
+++ booking-services/src/App.js
@@ -2,30 +2,34 @@ import "./App.css";
 import BPBAHome from "./pages/bpba/bpba-home";
 import Login from "./pages/login";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import DetailUserPba from "./pages/bpba/detail-user-pba";
 import Tambah from "./pages/bpba/tambahUser";
 import TambahLibur from "./pages/bpba/tambah-libur";
+import DetailUserPbam from "./pages/bpba/detail-user-pbam";
+import Edit from "./pages/bpba/editUser";
+import DaftarLibur from "./pages/bpba/daftar-libur";
+import EditLibur from "./pages/bpba/edit-libur";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
-          <Route path="/bpba/tambah-libur" exact>
-            <TambahLibur />
-          </Route>
-          <Route path="/bpba/tambah-user" exact>
-            <Tambah />
-          </Route>
-          <Route path="/bpba/detail-user/:id" exact>
-            <DetailUserPba />
-          </Route>
-          <Route path="/bpba/home" exact>
-            <BPBAHome />
-          </Route>
-          <Route path="/" exact>
-            <Login />
-          </Route>
+          <Route path="/bpba/edit-libur/:id" exact component={EditLibur} />
+          <Route path="/bpba/daftar-libur" exact component={DaftarLibur} />
+          <Route
+            path="/bpba/detail-user/edit-user/:id"
+            exact
+            component={Edit}
+          />
+          <Route path="/bpba/tambah-libur" exact component={TambahLibur} />
+          <Route path="/bpba/tambah-user" exact component={Tambah} />
+          <Route
+            path="/bpba/detail-user/:id"
+            exact
+            component={DetailUserPbam}
+          />
+          <Route path="/bpba/home" exact component={BPBAHome} />
+          <Route path="/" exact component={Login} />
         </Switch>
       </Router>
     </div>
